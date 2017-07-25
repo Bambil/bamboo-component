bf1ac2a1888bae01d3ae13993c61996cdb41cec4
Parham Alvani <parham.alvani@gmail.com>
2017-07-26 00:27:35 +0430

    Index file was added.

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 26-07-2017
+ * |
+ * | File Name:     index.js
+ * +===============================================
+ */
+module.exports = require('./src/component')
